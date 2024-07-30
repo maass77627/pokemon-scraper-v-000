c5e8f9b3e008f6ec64d4eed91f77b774fc6e250d
kelly_maass <kmaass77627@gmail.com>
2024-07-29 20:49:50 -0500

    done

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+
+end
+
+
+
+def self.save(name, type, db)
+    sql = <<-SQL
+    INSERT INTO pokemon (name, type)
+    VALUES (?, ?)
+    SQL
+    db.execute(sql, name, type)
+end
+
+def self.find(idnum, db)
+    sql = <<-SQL
+    SELECT *
+    FROM pokemon
+    WHERE id = ?
+    LIMIT 1
+    SQL
+    array = db.execute(sql, idnum).flatten
+    self.new(id: array[0], name: array[1], type: array[2], db: array[3])
+end
+
+
 end
